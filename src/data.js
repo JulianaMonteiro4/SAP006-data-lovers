2379201b05781f8aa9ede880b566f2955d62e6ec
juliana monteiro <juliana47monteiro@gmail.com>
2021-07-13 16:26:56 -0300

    Sobre a Animação

--- src/data.js
+++ src/data.js
@@ -1,7 +1,13 @@
-import data from "./data/ghibli/ghibli.json";
+import data from "./data/ghibli/ghibli.js";
 export const films = data.films;
-//export const imgFilm = films[0].poster;
 
-export const filterAni = () =>  {
-  return 'OMG';
+export const filterAni = (arr, key, value) => 
+    arr.filter((item) => item[key] === value);
+
+export const orderAni = (arr, order) => {
+    if (order === "A-Z"){
+        return arr.sort((a, z) => (a.title > z.title ? 1 : -1));
+    }else if (order === "Z-A") {
+        return arr.sort((a, z) => (a.title > z.title ? -1 : 1));
+    }
 };
--- src/main.js
+++ src/main.js
@@ -1,24 +1,49 @@
 import data from "./data/ghibli/ghibli.js"
+import { filterAni, orderAni } from "./data.js";
 
 // Primeiro filme apareça na tela
-const films = data.films ;
-//let imgFilm = films[0].poster;
-//console.log(imageFilm)
+const films = data.films;
+const orderChar = films[0].title ; 
 
-addEventListener("load", infoAnimaTela)
 function infoAnimaTela (films) {
-    document.getElementById("infoAnimacoes").innerHTML = films.map((film) => ` 
+    document.getElementById("infoAnimacoes").innerHTML = films.map((film) => `
+        <a class="cards" id="cards" href="./infoAni?info=${film.id}" target="_blank">
         <div class="imageFilm">
             <img src=${film.poster} class="cardImg" alt="Imagem da Animação">
             <div class="cardBack">
                 <h2>${film.title}</h2>
-                <p>${film.director}</p>
-                <p>${film.producer}</p>
-                <p>${film.release_date}</p>
+                <p> Diretor: ${film.director}</p>
+                <p> Produtor: ${film.producer}</p>
+                <p> Ano de Lançamento: ${film.release_date}</p>
+                <img src="img/pngegg.png" alt="Imagem Card">
             </div>
         </div>
+        </a>
     `).join("")
 }
 infoAnimaTela(films);
 
-//filtrooos
+//filtro
+function filterInfo (f) {
+    const value = f.target.value;
+    const nameOfRed = value.split(":");
+    if ( value.includes("director")) {
+        infoAnimaTela(filterAni(films, "director", nameOfRed[1]));
+    } else if ( value.includes("producer")) {
+        infoAnimaTela(filterAni(films, "producer", nameOfRed[1]));
+    } else if (value === "Filters") {
+        infoAnimaTela(films);
+    }
+}
+document.getElementById("filter").addEventListener("change", filterInfo);
+
+//ordenar
+function orderInfo (o) {
+    const value = o.target.value;
+    if (value === "sorter") {
+        infoAnimaTela(films);
+    } else {
+        infoAnimaTela(orderAni(films, value));
+    }
+}
+document.getElementById("order").addEventListener("change", orderInfo);
